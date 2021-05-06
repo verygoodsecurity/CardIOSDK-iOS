2a764795a837f259a9f50845fbd5fbba69b330bf
EugeneIOs <72962856+EugeneIOs@users.noreply.github.com>
2021-05-06 15:50:29 +0300

    Update project struct for SPM (#4)

---
+++ Sources/CardIOSDK/CardIOSDK.h
@@ -0,0 +1,19 @@
+//
+//  CardIOSDK.h
+//  CardIOSDK
+//
+//  Created by Dima on 07.02.2020.
+//  Copyright © 2020 DKH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardIOSDK.
+FOUNDATION_EXPORT double CardIOSDKVersionNumber;
+
+//! Project version string for CardIOSDK.
+FOUNDATION_EXPORT const unsigned char CardIOSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardIOSDK/PublicHeader.h>
+
+
